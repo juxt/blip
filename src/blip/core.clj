dfc1c854c492bb2375f1a246514deaadb8fcb24d
Dajana Herichova <dajana.herichova@gmail.com>
2023-01-04 17:13:25 +0100

    Changes for cljs part of the library to work with re-frame app
    * Removed cljs reader conditionals from core.cljs
    * Renamed core.cljs to core.clj
    * New query-helper.cljc namespace for common functions for cljs and clj parts
    of the library (process-query-definitions, prepare-query-body, extract-query-result)
    * New re-frame.cljc namespace to make graphql requests from re-frame app

---
+++ src/blip/core.clj
@@ -0,0 +1,54 @@
+(ns blip.core
+  (:require [clojure.string :as string]
+            [cheshire.core :as cheshire]
+            [clojure.java.io :as io]
+            [clj-http.lite.client :as http]
+            [blip.query-helper :as qh]))
+
+;; TODO: something more robust to check whether filename is url location
+(defmulti get-query-definition
+  (fn [file-name & _]
+    (if (string/starts-with? file-name "http")
+      :remote
+      :local)))
+
+(defn get-request [url & {:keys [headers]}]
+  (http/get url {:headers (if (delay? headers) @headers headers)}))
+
+(defn post-request [url body & {:keys [headers]}]
+  (let [{:keys [body]} (http/post
+                        url
+                        {:body    (cheshire/generate-string body)
+                         :headers (if (delay? headers) @headers headers)})]
+    (cheshire/parse-string body)))
+
+(defmethod get-query-definition :local
+  [file-name & _]
+  (-> file-name io/reader line-seq))
+
+(defmethod get-query-definition :remote
+  [file-name & [opts]]
+  (-> file-name (get-request opts) :body string/split-lines))
+
+;; Public API
+
+(defn load-queries
+  "Given graphql resource handle and site-auth map, fetches graphql resource
+  and calls `blip.query-helper/process-query-definitions` on it"
+  [file-name & [opts]]
+  (-> file-name (get-query-definition opts) qh/process-query-definitions))
+
+(def memoized-load-queries (memoize load-queries))
+
+(defn init
+  "Higher-order function which is called with a graphql resource handle as a first argument
+  (either local filename or remote URI) and map containing query `endpoint` for making requests,
+  plus optional parameters like HTTP headers.
+  Returns function which takes query/mutation name as a first and query-args as rest arguments
+  and performs the graphql request when called."
+  [gql-queries endpoint & [opts]]
+  (fn [query-name & query-args]
+    (let [query-val (get (memoized-load-queries gql-queries opts) query-name)
+          query {:query (second query-val)}]
+      (-> (post-request endpoint (qh/prepare-query-body query query-args) opts)
+          qh/extract-query-result))))
---
+++ src/blip/site.clj
@@ -0,0 +1,31 @@
+(ns blip.site
+  (:require [blip.core :as core]
+            [cheshire.core :as cheshire]
+            [clj-http.lite.client :as http]))
+
+(defn get-site-token
+  "Retrieve the site token necessary to authorize all other interactions."
+  [{:keys [endpoint username pass] :or {username "admin" pass "admin"} :as site-auth}]
+  (let [response (http/post
+                  endpoint
+                  {:headers {"content-type" "application/x-wWw-form-urlencoded"}
+                   :basic-auth [username pass]
+                   :body "grant_type=client_credentials"})
+        body (cheshire/parse-string (:body response))
+        access-token (get body "access_token")]
+    access-token))
+
+;; Public API
+
+(defn init
+  "Higher-order function which is called with graphql resource handle as a first argument
+  (either local filename or remote URI) and map containing query `endpoint` for making requests,
+  and `site-auth` map to specify site authentication.
+  Returns function which takes query/mutation name as a first and query-args as rest arguments
+  and performs the graphql request when called."
+  [gql-queries endpoint & {:keys [site-auth]}]
+  (let [headers (delay {"Content-Type" "application/json"
+                        "authorization" (str "Bearer " (get-site-token site-auth))})]
+    (core/init gql-queries endpoint {:headers headers})))
+
+
